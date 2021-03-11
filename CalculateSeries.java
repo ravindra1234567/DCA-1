a7eb8dd5d4a3f146f31d4bd158a65704d47a21d0
ravindra1234567 <ravindrakushwahanwg@gamil.com>
2021-03-12 00:05:44 +0530

    add dca program

---
+++ CalculateSeries.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class CalculateSeries {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        ArrayList<Integer> arr = new ArrayList<>();
+            int even =0;
+            int odd =1;
+            for (int i = 0; i <n; i++) {
+               if(i==0)
+                    arr.add(0);
+                else if(i==1)
+                    arr.add(0);
+                else if((i&1) !=1){
+                    even+=2;
+                    arr.add(even);
+                }
+                else if(i == 3){
+                    arr.add(odd);
+                }
+                else if((i&1)==1){
+                    odd+=2;
+                    arr.add(odd);
+                }
+
+            }
+      
+        System.out.println(arr.get(n-1));
+        scan.close();
+    }
+}
