2aa3737f96ea64902fd67917ca5625ffc54f71e3
ravindra1234567 <ravindrakushwahanwg@gamil.com>
2021-03-12 00:24:45 +0530

    add second method

--- CalculateSeries.java
+++ CalculateSeries.java
@@ -5,30 +5,38 @@ public class CalculateSeries {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
 
-        ArrayList<Integer> arr = new ArrayList<>();
-            int even =0;
-            int odd =1;
-            for (int i = 0; i <n; i++) {
-               if(i==0)
-                    arr.add(0);
-                else if(i==1)
-                    arr.add(0);
-                else if((i&1) !=1){
-                    even+=2;
-                    arr.add(even);
-                }
-                else if(i == 3){
-                    arr.add(odd);
-                }
-                else if((i&1)==1){
-                    odd+=2;
-                    arr.add(odd);
-                }
+        // ArrayList<Integer> arr = new ArrayList<>();
+        //     int even =0;
+        //     int odd =1;
+        //     for (int i = 0; i <n; i++) {
+        //        if(i==0)
+        //             arr.add(0);
+        //         else if(i==1)
+        //             arr.add(0);
+        //         else if((i&1) !=1){
+        //             even+=2;
+        //             arr.add(even);
+        //         }
+        //         else if(i == 3){
+        //             arr.add(odd);
+        //         }
+        //         else if((i&1)==1){
+        //             odd+=2;
+        //             arr.add(odd);
+        //         }
 
-            }
+        //     }
       
-        System.out.println(arr.get(n-1));
-        
+        // System.out.println(arr.get(n-1));
+
+        // second way 
+        if(n==0 || n==1)
+            System.out.println(0);
+        else if((n&1)!=1)
+            System.out.println(n-3);
+        else if((n&1)==1)
+            System.out.println(n-1);
+
         scan.close();
     }
 }
